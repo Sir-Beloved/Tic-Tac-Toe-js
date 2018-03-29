108c7edba9c2b473defd51bd6ecc7d337d62784a
Beloved <belovedadewole@gmail.com>
2018-03-29 19:22:09 +0100

    This is the last javascript update for the game

--- script.js
+++ script.js
@@ -117,3 +117,11 @@ function fnNewGame() {
         gameUL.appendChild(tr);
     }
 }
+
+
+function isEven(value) {
+    if (value % 2 == 0)
+        return true;
+    else
+        return false;
+}
