c5926d9245423a99988402c03e9a4c3fcf505064
Beloved <belovedadewole@gmail.com>
2018-03-29 18:55:04 +0100

    This is an update in the css and javascript

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ var score = {
     'O': 0
 };
 var gridValue = 0;
+function fnLoad() {
+    var select = document.getElementById("grid");
+    for (i = 3; i <= 100; i += 1) {
+        var option = document.createElement('option');
+        select.options[select.options.length] = new Option(i + ' X ' + i, i);
+    }
+
+    addEvent(document.getElementById("game"), "click", fnChoose);
+
+    fnNewGame();
+}
